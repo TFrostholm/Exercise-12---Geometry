6f63ab49aa49d728d37072de306936ce32567f4e
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-20 13:42:39 +0100

    Finished

--- Sandbox/Geometry.cs
+++ Sandbox/Geometry.cs
@@ -7,5 +7,61 @@ namespace Sandbox
 {
     public class Geometry
     {
+        private double length, hight, radius, angle1, angle2, angle3, angle4;
+
+        public double perimeter, area, circleArea;
+        public bool isRectangle;
+
+        //Creating the rectangle
+        public void Rectangle(double sideA, double sideB)
+        {
+            this.length = sideA;
+            this.hight = sideB;
+        }
+        
+        public double CalculatePerimeterOfRectangle()
+        {
+            perimeter = length*2 + hight*2;
+            return perimeter;
+        }
+
+        public double CalculateAreaOfRectangle()
+        {
+            area = length*hight;
+            return area;
+        }
+
+        public void Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateAreaOfCircle()
+        {
+            circleArea = Math.PI*(radius*radius);
+            return circleArea;
+        }
+
+        public void Polygon(double angle1, double angle2, double angle3, double angle4)
+        {
+            this.angle1 = angle2;
+            this.angle2 = angle2;
+            this.angle3 = angle3;
+            this.angle4 = angle4;
+        }
+
+        public bool IsPolygonRectangle()
+        {
+            if (angle1 == 90 && angle2 == 90 && angle3 == 90 && angle4 == 90)
+            {
+                isRectangle = true;
+            }
+            else
+            {
+                isRectangle = false;
+            }
+            return isRectangle;
+
+        }
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,8 +7,21 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+             Geometry geometry = new Geometry();
 
+            geometry.Rectangle(10, 10);
+            geometry.CalculatePerimeterOfRectangle();
+            geometry.CalculateAreaOfRectangle();
 
+            Console.WriteLine("The perimeter of the rectangle is: {0} and the area is: {1}", geometry.perimeter, geometry.area);
+
+            geometry.Circle(3);
+            geometry.CalculateAreaOfCircle();
+            Console.WriteLine("The perimeter of the circle is: {0}", geometry.circleArea);
+
+            geometry.Polygon(90, 90, 90, 90);
+            geometry.IsPolygonRectangle();
+            Console.WriteLine("The polygon is a rectangle? = {0}", geometry.isRectangle);
             // The LAST line of code should be ABOVE this line
         }
     }
